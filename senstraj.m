f3c42e95e28c6b0d2492b06897ba86f9e2680b79
Fabian Arzberger <fabian.arzberger@uni-wuerzburg.de>
2024-02-26 16:04:23 +0100

    Integrating rotation via gyro alone sucks. Using pre-filtered orientation (accelerometer included). Only use rotation speed as position estimate

--- senstraj.m
+++ senstraj.m
@@ -3,12 +3,9 @@ rs = 0.145; % m, radius of sphere
 d = [0.00; 0.04; -0.07]; % m, extrinsic parameters Center -> Sensor 
 % Normal vector of plane wrt. global frame
 n = [0;0;-1]; % rotation around this axis will not result in translation.
-% Initial angle in global reference frame
-a0 = [120;0;0]*pi/180; % radians! eul2rotm wants rad...
 
 % Input signal (angular velocity) in spheres frame of reference
-%bag = rosbag('/home/fabi/Documents/bagfiles/Fabi_Wiecha_BA/280922_optitrack/6/optitrack6.bag');
-bag = rosbag('/home/fabi/Documents/bagfiles/2023-03-13-11-28-24.bag');
+bag = rosbag('/home/fabi/Documents/bagfiles/TimRolling/20_02_large01.bag');
 topic = select(bag, 'Time', [bag.StartTime, bag.EndTime], 'Topic', '/orientation');
 msgs = readMessages(topic, 'DataFormat', 'struct');
 nsteps = length(msgs);
@@ -25,11 +22,20 @@ for i = 1:nsteps-1
     end
     dt(1,i) = 1e-9 * dt(1,i); % convert to seconds
 end
+% Timestamps
 ts(1,nsteps) = t(1,nsteps)*1e-9 + countNumSec; %trailing zero
+% Gyro vals
 wx = cellfun( @(m) double(m.AngularVelocity.X), msgs)';
 wy = cellfun( @(m) double(m.AngularVelocity.Y), msgs)';
 wz = cellfun( @(m) double(m.AngularVelocity.Z), msgs)';
 w = [wx; wy; wz]*180/pi; % given in degrees/s
+% Pre-filtered orientation
+qx = cellfun( @(m) double(m.Orientation.X), msgs)';
+qy = cellfun( @(m) double(m.Orientation.Y), msgs)';
+qz = cellfun( @(m) double(m.Orientation.Z), msgs)';
+qw = cellfun( @(m) double(m.Orientation.W), msgs)';
+% conversion from ros to matlab quaternions:
+q = [-qw; qx; qy; qz];
 
 %% Internal variables
 dr = zeros(3,nsteps); % Position of sensor wrt. center of sphere
@@ -38,17 +44,14 @@ c = zeros(3,nsteps); % Position of the center wrt. global frame
 pos = zeros(3,nsteps); % Position of the sensor wrt. global frame
 alpha = cell(1,nsteps); % Orientation of the ball 
 % Initial conditions
-% eul2rotm wants radiant angles.
-alpha{1,1} = eul2rotm([a0(1) a0(2) a0(3)],'XYZ');
+alpha{1,1} = quat2rotm(q(:,1)');
 dr(:,1) = alpha{1,1}\d;
 pos(:,1) = dr(:,1) + c(:,1);
 v(:,1) = cross(alpha{1,1}\(rs*w(:,1)*pi/180), n/norm(n));
-
 %% Simulation
 for k = 1:1:nsteps-1 
-    delR = eul2rotm((w(:,k)*pi/180)'*dt(:,k), 'XYZ');
     % Rotate sensor in fixed sphere-center frame
-    alpha{1,k+1} = delR*alpha{1,k};
+    alpha{1,k+1} = quat2rotm(q(:,k+1)');
     % Integration of gyro measurement
     dr(:,k+1) = alpha{1,k+1}\d;
     % Linear velocity (Roll without slip)
